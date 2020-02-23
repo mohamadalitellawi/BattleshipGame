8c580dcb68f2370ce4403f58eba7e7a106c771f9
mohamad ali <mohamadalitellawi@gmail.com>
2020-02-23 22:15:27 +0300

    in progress

---
+++ BattleshipLibrary/GameLogic.cs
@@ -0,0 +1,47 @@
+﻿using BattleshipLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipLibrary
+{
+    public static class GameLogic
+    {
+        public static void InitializeGrid (PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A","B","C","D","E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1,2,3,4,5
+            };
+
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+            model.ShotGrid.Add(spot);
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            return true;
+        }
+    }
+}
--- BattleshipUI/Program.cs
+++ BattleshipUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BattleshipLibrary;
+using BattleshipLibrary.Models;
 
 namespace BattleshipUI
 {
@@ -6,7 +8,61 @@ namespace BattleshipUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WelcomeMessage();
+
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome To BattleShip");
+            Console.WriteLine("Created By MAT\n");
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
+            
+            output.UserName = AskForUsersName();
+
+            GameLogic.InitializeGrid(output);
+
+            PlaceShips(output);
+
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place your ship number {model.ShipLocations.Count + 1} : ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation ==false)
+                {
+                    Console.WriteLine("That was not a valid location. please try again.");
+                }
+
+
+            } while (model.ShipLocations.Count < 5);
         }
     }
 }
